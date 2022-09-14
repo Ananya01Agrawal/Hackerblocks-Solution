838dcc70f3293c4892f2fd9f9eb9a71935a6df4a
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-09-14 09:16:10 +0530

    Create Inverse of number.java

---
+++ Inverse of number.java
@@ -0,0 +1,53 @@
+/*
+Take the following as input.
+A number
+Assume that for a number of n digits, the value of each digit is from 1 to n and is unique. E.g. 32145 is a valid input number.
+
+Write a function that returns its inverse, where inverse is defined as follows
+
+Inverse of 32145 is 12543. In 32145, “5” is at 1st place, therefore in 12543, “1” is at 5th place; in 32145, “4” is at 2nd place, therefore in 12543, “2” is at 4th place.
+
+Print the value returned.
+
+Input Format
+Integer
+
+Constraints
+0 < N < 1000000000
+
+Output Format
+Integer
+
+Sample Input
+32145
+Sample Output
+12543
+*/
+
+//code
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int place = 1;
+    int mult = 1;
+    int ans = 0;
+
+    while(num != 0){
+        int rem = num % 10;
+        ans = ans + place * (int)Math.pow(10, rem - 1);
+        num = num / 10;
+        place++;
+    }
+    System.out.println(ans);
+    }
+}
+    
+
+
+
+
+
+
+  

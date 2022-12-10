963241950260d7d51a814797e23b05fa705d8280
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-12-10 10:23:12 +0530

    Create Library Impasse.java

---
+++ Test Questions/Library Impasse.java
@@ -0,0 +1,31 @@
+import  java.util.Scanner;
+        
+         public   class  Main {  
+             public   static   void  main(String[] args) {  
+                Scanner sc =  new  Scanner(System.in);  
+                int n = sc.nextInt();  
+                int[] arr =  new   int [n];  
+                 for  ( int  i = 0; i < n; i++) {  
+                    arr[i] = sc.nextInt();  
+                }  
+                int[] left =  new   int [n];  
+                int[] right =  new   int [n];  
+                left[0] = arr[0];  
+                right[n - 1] = arr[n - 1];  
+                 for  ( int  i = 1; i < n; i++) {  
+                    left[i] = left[i - 1] + arr[i];  
+                }  
+                 for  ( int  i = n - 2; i >= 0; i--) {  
+                    right[i] = right[i + 1] + arr[i];  
+                }  
+                 for  ( int  i = 0; i < n; i++) {  
+                     if  (left[i] == right[i]) {  
+                        System.out.println(i);  
+                         return ;  
+                    }  
+                }  
+                System.out.println( "-1" );  
+            }  
+        }
+        
+
